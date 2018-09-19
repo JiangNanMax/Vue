af45259c26e55fb1c78ef74343d2b6eeb25fb09d
jiangnan <18918020032@163.com>
2018-09-19 19:48:39 +0800

    vue

---
+++ AwesomePos/src/components/common/leftNav.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="leftnav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-goumai"></i>
+                <div class="">收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dianpu"></i>
+                <div class="">店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-hanbao"></i>
+                <div class="">商品</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-huiyuanqia"></i>
+                <div class="">会员</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-37"></i>
+                <div class="">统计</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-gongnengjianyi"></i>
+                <div class="">设置</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'leftNav',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+    .leftnav{
+        color: #fff;
+        font-size: 10px;
+        height: 100%;
+        background-color: #1D8CE0;
+        float: left;
+        width: 5%;
+    }
+    .iconfont{
+        font-size: 25px;
+    }
+    .leftnav ul{
+        padding: 0px;
+        margin: 0px;
+    }
+    .leftnav li{
+        list-style: none;
+        text-align: center;
+        border-bottom: 1px solid #20a0ff;
+        padding: 10px;
+    }
+</style>
---
+++ AwesomePos/src/components/page/Pos.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="pos">
+        <div class="">
+            <el-row>
+                <el-col :span='6' class="pos-order" id="order-list">
+                    <el-tabs>
+                        <el-tab-pane label="点餐">
+                            <el-table :data="tableData" border show-summary style="width: 100%">
+                                <el-table-column prop="goodsName" label="商品" width="100"></el-table-column>
+                                <el-table-column prop="count" label="数量" width="50"></el-table-column>
+                                <el-table-column prop="price" label="金额" width="50"></el-table-column>
+                                <el-table-column prop="" label="操作" width="100" fixed="right">
+                                    <template scope="scope">
+                                        <el-button type="text" size="small">删除</el-button>
+                                        <el-button type="text" size="small">增加</el-button>
+                                    </template>
+                                </el-table-column>
+                            </el-table>
+                            <div class="">
+                                <el-button>结算</el-button>
+                                <el-button>挂单</el-button>
+                                <el-button>删除</el-button>
+                            </div>
+                        </el-tab-pane>
+
+                        <el-tab-pane label="挂单">
+
+                        </el-tab-pane>
+
+                        <el-tab-pane label="外卖">
+
+                        </el-tab-pane>
+                    </el-tabs>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'pos',
+  data(){
+      return{
+          tableData: [{
+          goodsName: '可口可乐',
+          price: 8,
+          count:1
+        }, {
+          goodsName: '香辣鸡腿堡',
+          price: 15,
+          count:1
+        }, {
+          goodsName: '薯条',
+          price: 8,
+          count:1
+        }, {
+          goodsName: '甜筒',
+          price: 8,
+          count:1
+        }]
+      }
+  },
+  mounted: function(){
+      var orderHeight = document.body.clientHeight;
+      console.log(orderHeight);
+      document.getElementById("order-list").style.height=orderHeight+'px';//!!!
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.pos-order {
+    background-color: #F9FAFC;
+    border-right: 1px solid #C0CCDA;
+    height: 100%;
+}
+</style>
